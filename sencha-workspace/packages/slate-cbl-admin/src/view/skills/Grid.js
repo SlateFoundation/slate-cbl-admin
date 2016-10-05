e5753295507c4e098ed1ef42bdc747dc064b991b
Nafis Bey <nafis@jarv.us>
2016-10-05 10:35:46 -0400

    Make grid "infinite"

--- sencha-workspace/packages/slate-cbl-admin/src/view/skills/Grid.js
+++ sencha-workspace/packages/slate-cbl-admin/src/view/skills/Grid.js
@@ -1,25 +1,16 @@
 Ext.define('Slate.cbl.admin.view.skills.Grid', {
     extend: 'Ext.grid.Panel',
     requires: [
-        'Ext.toolbar.Paging',
-        'Ext.grid.plugin.CellEditing'
+        'Ext.grid.plugin.BufferedRenderer'
     ],
 
-    xtype: 'slate-cbl-admin-skillsgrid',
+    xtype: 'cbl-admin-skills-grid',
 
     store: 'Skills',
     height: "100%",
 
-    dockedItems: [{
-        xtype: 'pagingtoolbar',
-        store: 'Skills',
-        dock: 'bottom',
-        displayInfo: true
-    }],
-
     plugins: {
-        ptype: 'cellediting',
-        clicksToEdit: 2
+        ptype: 'bufferedrenderer'
     },
 
     columns: {
@@ -43,34 +34,6 @@ Ext.define('Slate.cbl.admin.view.skills.Grid', {
             flex: 5,
             editor: {
                 xtype: 'textfield',
-                allowBlank: false
-            }
-        },{
-            dataIndex: 'FirstLevelDemonstrationsRequired',
-            text: 'ER\'S Level 9',
-            editor: {
-                xtype: 'numberfield',
-                allowBlank: false
-            }
-        },{
-            dataIndex: 'SecondLevelDemonstrationsRequired',
-            text: 'ER\'S Level 10',
-            editor: {
-                xtype: 'numberfield',
-                allowBlank: false
-            }
-        },{
-            dataIndex: 'ThirdLevelDemonstrationsRequired',
-            text: 'ER\'S Level 11',
-            editor: {
-                xtype: 'numberfield',
-                allowBlank: false
-            }
-        },{
-            dataIndex: 'FourthLevelDemonstrationsRequired',
-            text: 'ER\'S Level 12',
-            editor: {
-                xtype: 'numberfield',
                 allowBlank: false
             }
         }]
